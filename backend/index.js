188c0094e3e81e423e016dbf1c901348f53fbfb1
Therings <shucai@live.cn>
2024-01-25 19:44:03 +0200

    Backend

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const tourRoute = require("./routes/tours.js");
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
+// database connection
+mongoose.set("strictQuery", false);
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDB");
+  } catch (err) {
+    console.log("MongoDB Connection Failed");
+  }
+};
+// use middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use("/tours", tourRoute);
+
+// for testing
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  connect();
+  console.log(`Server listening at http://localhost:${port}`);
+});
