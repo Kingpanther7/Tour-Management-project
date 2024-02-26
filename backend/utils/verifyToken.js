09ce9155c6e19c67a18b68b20bd691920c13bf82
chen <shucai@live.cn>
2024-02-26 22:16:31 +0200

    Update verifyToken.js

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
 
-  if (token) {
+  if (!token) {
     return res.status(401).json({ message: "Dont have token" });
   }
 
